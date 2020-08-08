dc659f56aad5e04230a6c29dc539556f76bc8cbf
DS Rahul <45290221+dsr-11@users.noreply.github.com>
2020-08-08 11:13:01 +0530

    for Videos

--- Face Mask det.py
+++ Face Mask det.py
@@ -1,28 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import pickle
 model = pickle.load(open('finalized_model.sav', 'rb'))
-
-
-# In[2]:
-
 
 import cv2
 import numpy as np
 
-
-# In[3]:
-
-
-
 face_clsfr=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-source=cv2.VideoCapture(r'C:\Users\ASUS\Pictures\Camera Roll\abc.mp4')
-
+source=cv2.VideoCapture('path') # 0 for live camera and path for precorded video
 labels_dict=['MASK','NO MASK']
 color_dict={0:(0,255,0),1:(0,0,255)}
 ret,img=source.read()
@@ -51,57 +37,3 @@ while(True):
         break
 source.release()       
 cv2.destroyAllWindows()
-
-
-# In[ ]:
-
-
-from keras.preprocessing.image import load_img, img_to_array
-from numpy import asarray
-from PIL import Image
-import numpy as np
-import matplotlib.pyplot as plt
-img = load_img('adi.jpg', target_size=(128,128))
-plt.imshow(img)
-img1 = img_to_array(img)
-img1 = img1.reshape(1,128,128,3)
-res = ["Without Mask","With Mask"]
-preds = model.predict(img1)
-plt.imshow(img)
-plt.title("Predicted: {},   Actual: {}".format(res[np.argmax(preds)], res[1]))
-
-
-# In[ ]:
-
-
-vid = cv2.VideoCapture(0) 
-  
-while(True): 
-      
-    # Capture the video frame 
-    # by frame 
-    ret, frame = vid.read() 
-    # Display the resulting frame 
-    cv2.imshow('frame', frame) 
-      
-    # the 'q' button is set as the 
-    # quitting button you may use any 
-    # desired button of your choice 
-    if cv2.waitKey(1) & 0xFF == ord('q'): 
-        break
-vid.release() 
-# Destroy all the windows 
-cv2.destroyAllWindows() 
-
-
-# In[ ]:
-
-
-print(np.argmax([0,1]))
-
-
-# In[ ]:
-
-
-
-
